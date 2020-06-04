50c9b8d61fbd89e731f76933bd611cb6ca738258
Willem <38408005+wkapenda@users.noreply.github.com>
2020-06-04 10:47:12 +0200

    Update CuRecovery.py
    Final Update

--- CuRecovery.py
+++ CuRecovery.py
@@ -1,134 +1,93 @@
+# Copper Recovery Prediction using KNN regression model
+# Author: Willem Kapenda
+# Date: 15 May 2020
+# Version: Final
+
+
+# import openpyxl library to read from xlsx file
+import openpyxl as op
 # data analysis library
 import pandas as pd
-# scientific computing library
+# scientific computing library, arrays
 import numpy as np
-# library to Read/Write to xlsx file
-import openpyxl as op
 
-import tensorflow as tf
-
+# import scikit-learn module used for the KNN regression model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
-import matplotlib.pyplot as plt
-
-from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsRegressor
 
 
+# initialize the KNN regressor model
+knn = KNeighborsRegressor()
 # initialize the scaling function
 sc = StandardScaler()
 
+# DATA EXTRACTION
+
 # store xlsx file name into string variable
 excel_file = 'DSZinc.xlsx'
-
 # load the excel file into a book
 dataBook = op.load_workbook(excel_file)
-
 # activate the book as a worksheet
 dataSheet = dataBook.active
 
-# convert worksheet into a data frame including the column names
+# convert datasheet into a dataframe including the column names
 df = pd.DataFrame(dataSheet.values, columns=['Date','Cu(hg)','Pb(hg)','Zn(hg)','Ag(hg)','Delivered to Plant',
 'Milled','% Cu','Cu Recovery','% Pb', 'Pb Recovery', '% Zn','Zn Recovery','tons Ag','Ag Recovery','Source',
 'Cu in Cu','Pb in Cu','Zn in Cu','Ag in Cu','pH','Oil in feed','+150 microns','eH'])
 
+
 # DATA CLEANING
 
-# delete 2nd row because it contains column names
+# delete 1st row because it contains column names which are already defined
 df = df.drop([0], axis=0)
-# delete last row with averages
+# calculate the length of the dataframe and delete the last row containing averages
 total_rows = len(df.index)
 df = df.drop([total_rows], axis=0)
 
-# delete the date column
+# delete the Date and Source columns
 df = df.drop(["Date", "Source"], axis=1)
 
-# number of missing values Pandas can detect
-#print df.isnull().sum().sum()
-
-# replace all entries with 0 by None, 0 not detected by Pandas as a missing value
+# replace all 0 entries with by None, because 0 is not detected by Pandas as a missing value
 df = df.replace({0: None})
 
-# delete rows which are fully empty
+# delete rows which are fully empty, which contain only None value
 df = df.dropna(how='all')
 
 # replace empty entries with zero
-df = df.dropna()
-#df = df.replace({None: 0})
+df = df.replace({None: 0})
 
-# create target
+# create target dataframe
 selected_column = df[["Cu Recovery"]]
 target = selected_column.copy()
 
+# create the features dataframe by removing columns deemed to be redundant
+features = df.drop(['Cu Recovery','Oil in feed','eH'], axis=1)
 
-# create features by deleting the Cu Recovery, Oil in feed, +150 microns and eH columns
-features = df.drop(["Cu Recovery",'tons Ag','Ag Recovery','Oil in feed','+150 microns','eH'], axis=1)
-
-
-
-kmeans = KMeans(n_clusters=18).fit(features)
-centroids = kmeans.cluster_centers_
-print(centroids)
-
-plt.scatter(df['x'], df['y'], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
-plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
-plt.show()
+# create array containing feature labels
+features_labels = [['Cu(hg)','Pb(hg)','Zn(hg)','Ag(hg)','Delivered to Plant','Milled','% Cu','% Pb','Pb Recovery',
+                    '% Zn','Zn Recovery','tons Ag','Ag Recovery','Cu in Cu','Pb in Cu','Zn in Cu','Ag in Cu',
+                    'pH','+150 microns']]
 
 
+# MODELLING AND TRAINING DATA
 
-# now add this code to the next cell and run to split your training and testing data to the specified ratio
-#features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.2)
-# validation data
-#features_train, features_validation, target_train, target_validation = train_test_split(features_train, target_train, test_size=0.2)
+# scale the features dataframe using the standard scaler fit transform function
+for x in features_labels:
+    features[x] = sc.fit_transform(features[x])
 
-#print features_train
+# split feature datframe into training (80%) and testing (20%) data to the specified ratio
+features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.2, random_state=4)
 
-# ML model, creating layers
-#model.add(Dense(16, input_dim=16, activation='relu'))
-#model.add(Dense(16, activation='relu'))
-#model.add(Dropout(0.5))
-#model.add(Dense(16, activation='relu'))
-#model.add(Dense(1, activation=None))
+# fit the knn model with the training data
+knn.fit(features_train, target_train)
+# predict the copper recoveries using the test data
+predictions = knn.predict(features_test)
 
-#print features
+# ANALYSIS
 
-# to scale the training set and the test set, add this code to the notebook cell and run it:
-#features_train = sc.fit_transform(features_train)
-#features_test = sc.fit_transform(features_test)
-#target_train = sc.fit_transform(target_train)
-#target_test = sc.fit_transform(target_test)
+# Model score, how often is the classifier correct?
+score = knn.score(features_test, target_test)
+print score*100
 
-# apply a gradient descent to the neural network
-#model.compile(optimizer= "adam",loss = "binary_crossentropy",metrics = ["accuracy"])
-
-# train model using to your the training data
-#model.fit(features_train, target_train, batch_size=10, epochs=10)
-
-#validation_data=(features_validation, target_validation))
-
-
-
-#prediction_features = model.predict(features_test)
-#performance = model.evaluate(features_test, target_test)
-
-#print prediction_features
-
-
-#print features_train
-#print features_train.shape
-#print target_train
-#print target_train.shape
-#print features_test.shape
-#print target_test.shape
-#print features_validation.shape
-#print target_validation.shape
-
-
-#print target.values
-#print target.dtypes
-#print df.isnull()
-#print df.isnull().sum()
-#print features.head(n=60)
-#print features.loc[2]
-#print target
-#print (features["Pb(hg)"])
+# The knn model has an accuracy of 82.07%
